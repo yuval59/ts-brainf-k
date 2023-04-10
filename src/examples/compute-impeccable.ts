63117a5613a2c5de50c109270f12e34e5b3688dc
yuval59 <yuvalmaron222@gmail.com>
2023-04-10 18:42:43 +0300

    Added new example

---
+++ src/examples/compute-impeccable.ts
@@ -0,0 +1,23 @@
+import { Program } from '../interpreter/program'
+
+const programCode = `[impeccable.b -- compute impeccable numbers
+(c) 2016 Daniel B. Cristofani
+http://brainfuck.org/]
+
+>>>->+[
+    [
+        [<<+>+>-]++++++[<<++++++++>>-]<<-.[-]<
+    ]++++++++++.[-]>>>++<[
+        [-[[>>>]<<<-[+>>>]<<<[<<<]+>]<-[+>++++++++++>>]>]>>>
+        [[>+<-]>>>]<<[-[<++>-[<++>-[<++>-[<++>-[<[-]>-[<++>-]>>[<+<]>[->]<++<<]]]]]<+<<]
+    >
+    ]>[>>>]<<<
+]
+
+This program outputs sequence (http://oeis.org/A014221). Although this sequence
+is technically nonterminating, computing its eighth term would require more
+storage than can exist in this universe, so you may as well kill this program
+after the seventh term.
+`
+
+new Program(programCode)
